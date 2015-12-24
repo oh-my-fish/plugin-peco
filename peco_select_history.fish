23059166c7a63e2423c9b6475468de07496ca6b1
Zoltan Nagy <abesto@abesto.net>
2015-12-24 15:32:12 +0100

    If no argument is specified, use the current commandline as the query

--- peco_select_history.fish
+++ peco_select_history.fish
@@ -1,6 +1,6 @@
 function peco_select_history
   if test (count $argv) = 0
-    set peco_flags --layout=bottom-up
+    set peco_flags --layout=bottom-up --qeury "(commandline)"
   else
     set peco_flags --layout=bottom-up --query "$argv"
   end
